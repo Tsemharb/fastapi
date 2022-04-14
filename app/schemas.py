9273d7176c3359a448f79a776f1a7345cab87d1d
tsemharb <gorbachevart@gmail.com>
2022-04-14 20:19:55 +0300

    Voting system

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
 from pydantic.typing import Optional
 from datetime import datetime
+from pydantic.types import conint
 
 
 class PostBase(BaseModel):
@@ -47,3 +48,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
